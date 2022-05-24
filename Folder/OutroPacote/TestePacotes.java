c284278a7d89ebd7b627e0aa90be985103c3b9c0
Lobo <78517083+ilessalobo@users.noreply.github.com>
2022-05-24 13:50:31 -0400

    Update TestePacotes.java

--- Folder/OutroPacote/TestePacotes.java
+++ Folder/OutroPacote/TestePacotes.java
@@ -7,26 +7,26 @@ public class TestePacotes {
 
 	public static void main(String[] args) {
 		
-		/*Acessa o mÈtodo da classe com atributos public*/
+		/*Acessa o m√©todo da classe com atributos public*/
 		Forma f = new Forma();
 		f.lado = 5.5;
 		f.getArea();
 		
-		/*N„o acessa o mÈtodo da classe com atributos private ou mÈtodos private*/
+		/*N√£o acessa o m√©todo da classe com atributos private ou m√©todos private*/
 //		FormaOutra fo = new FormaOutra();
 //		fo.lado = 5.5;
 //		fo.getArea();
 		
-		/*Acessa o mÈtodo da classe com atributos protecteds neste caso se a classe estive em mesmo pacote*/
+		/*Acessa o m√©todo da classe com atributos protecteds neste caso se a classe estiver em mesmo pacote*/
 		Triangulo t = new Triangulo();
 		t.lado = 5.5;
 		t.getArea();
 		
-		/*Conseguir· pegar atributos private acessando pelos mÈtodos getters e setters que est„o publicos*/ 
+		/*Conseguir√° pegar atributos private acessando pelos m√©todos getters e setters que est√£o publicos*/ 
 		Quadrado q = new Quadrado();
 		q.getLado();
 		q.setLado(5.5);
-		/*Mas n„o consegue acessar o mÈtodo private da classe*/
+		/*Mas n√£o consegue acessar o m√©todo private da classe*/
 		
 		}
 	
